d833617b72e4883a89830a0c923e0fd95f2724e5
aditekaw <adityaekaw21@gmail.com>
2023-06-20 15:25:26 +0700

    Membuat Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { images } from '../constants';
+
+function Header() {
+  return (
+  <section>
+    <header>
+        <div>
+            <img src={images.Logo} alt="logo"/>
+        </div>
+        <div>
+
+        </div>
+    </header>
+  </section>
+  );
+};
+
+export default Header;
